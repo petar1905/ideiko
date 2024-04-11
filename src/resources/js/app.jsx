7d40cbf8d0be0580ddc99040638e97e37d7351c1
Petar Vladov <60221587+petar1905@users.noreply.github.com>
2024-04-11 09:03:23 +0300

    Create placeholder theme for MUI
    This may or may not change in later stages of development.

--- src/resources/js/app.jsx
+++ src/resources/js/app.jsx
@@ -4,8 +4,27 @@ import '../css/app.css';
 import { createRoot } from 'react-dom/client';
 import { createInertiaApp } from '@inertiajs/react';
 import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
 
 const appName = import.meta.env.VITE_APP_NAME || 'Laravel';
+
+const theme = createTheme({
+    palette: {
+      primary: {
+        light: '#757ce8',
+        main: '#3f50b5',
+        dark: '#002884',
+        contrastText: '#fff',
+      },
+      secondary: {
+        light: '#ff7961',
+        main: '#f44336',
+        dark: '#ba000d',
+        contrastText: '#000',
+      },
+    },
+  });
+  
 
 createInertiaApp({
     title: (title) => `${title} - ${appName}`,
@@ -13,7 +32,11 @@ createInertiaApp({
     setup({ el, App, props }) {
         const root = createRoot(el);
 
-        root.render(<App {...props} />);
+        root.render(
+        <ThemeProvider theme={theme}>
+            <App {...props}/>
+        </ThemeProvider>
+        );
     },
     progress: {
         color: '#4B5563',
