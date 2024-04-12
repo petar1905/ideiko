200e76e0a865bbdb88c60fa2f1114b03670bbbd8
Petar Vladov <60221587+petar1905@users.noreply.github.com>
2024-04-12 20:27:44 +0300

    Write test for following

---
+++ src/tests/Feature/FollowTest.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+use App\Models\User;
+
+class FollowTest extends TestCase
+{
+    /**
+     * A basic feature test example.
+     */
+    public function test_follow(): void
+    {
+        $popular_user = User::factory()->create();
+
+        $first_follower = User::factory()->create();
+        $first_follower->followed()->save($popular_user);
+
+        $second_follower = User::factory()->create();
+        $second_follower->followed()->save($popular_user);
+
+        $this->assertEquals(2, count($popular_user->followers));
+    }
+}
