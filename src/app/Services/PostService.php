4b03c21369f9b9df83c799d37b27b6d0d2638f8b
Petar Vladov <60221587+petar1905@users.noreply.github.com>
2024-05-09 19:42:35 +0300

    Added missing methods for getting posts
    getPosts() and getLikedPosts() are used in the UserController class.

--- src/app/Services/PostService.php
+++ src/app/Services/PostService.php
@@ -1,5 +1,6 @@
 <?php
 namespace App\Services;
+use App\Models\User;
 use App\Models\Post;
 class PostService {
     public static function getAllPosts()
@@ -7,6 +8,23 @@ class PostService {
         $posts = Post::query()
         ->join('users', 'posts.user_id', '=', 'users.id')
         ->select('posts.*', 'users.name as user_name')
+        ->get();
+        return $posts;
+    }
+    public static function getPosts(User $user) {
+        $posts = Post::query()
+        ->select('posts.*', 'users.name as user_name')
+        ->where('user_id', '=', $user->id)
+        ->join('users', 'posts.user_id', '=', 'users.id')
+        ->get();
+        return $posts;
+    }
+
+    public static function getLikedPosts(User $user) {
+        $posts = $user->likedPosts()
+        ->select('posts.*', 'users.name as user_name')
+        ->where('posts.user_id', '=', $user->id)
+        ->join('users', 'posts.user_id', '=', 'users.id')
         ->get();
         return $posts;
     }
