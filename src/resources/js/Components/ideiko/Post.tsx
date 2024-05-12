b67b29e02de99815dfb02842370390c667f21a35
Petar Vladov <60221587+petar1905@users.noreply.github.com>
2024-05-12 14:07:17 +0300

    Refactored Post component

--- src/resources/js/Components/ideiko/Post.tsx
+++ src/resources/js/Components/ideiko/Post.tsx
@@ -3,6 +3,7 @@ import Box from '@mui/material/Box';
 import Card from '@mui/material/Card';
 import CardActions from '@mui/material/CardActions';
 import CardContent from '@mui/material/CardContent';
+import CardHeader from '@mui/material/CardHeader';
 import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
 import IconButton from '@mui/material/IconButton';
@@ -16,21 +17,23 @@ import { Avatar } from '@mui/material';
 
 import { Link } from '@inertiajs/react';
 export default function Post({post}) {
+    const avatar = (
+        <Avatar
+        alt={post.user_name}
+        src={`/static/images/avatar/${post.user_id}.jpg`}
+        />
+    );
   return (
     <Box sx={{ minWidth: 275 }}>
     <Card variant="outlined">
+        <Link href={`/room?id=${post.user_id}`}>
+            <CardHeader
+            avatar={avatar} 
+            title={post.user_name}
+            subheader={post.created_at}>
+            </CardHeader>
+        </Link>
         <CardContent>
-            <Link href={`/room?id=${post.user_id}`}>
-                <Box display={'flex'} columnGap={'.5em'} marginBottom={'.5em'}>
-                    <Avatar
-                    alt={post.user_name}
-                    src={`/static/images/avatar/${post.user_id}.jpg`}
-                    />
-                    <Typography paddingTop={'.6em'}>
-                        {post.user_name}
-                    </Typography>
-                </Box>
-            </Link>
             <Typography variant="body2">
                 {post.content}
             </Typography>
