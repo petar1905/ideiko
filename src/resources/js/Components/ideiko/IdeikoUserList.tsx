ffa4adc0c0c273188dbb0a9f25f8f85da8de61cc
Petar Vladov <60221587+petar1905@users.noreply.github.com>
2024-05-13 09:41:26 +0300

    User List code cleanup

--- src/resources/js/Components/ideiko/IdeikoUserList.tsx
+++ src/resources/js/Components/ideiko/IdeikoUserList.tsx
@@ -10,25 +10,9 @@ import Avatar from '@mui/material/Avatar';
 import { Link } from '@inertiajs/react';
 
 export default function IdeikoUserList({users}) {
-  const [checked, setChecked] = React.useState([1]);
-
-  const handleToggle = (value: number) => () => {
-    const currentIndex = checked.indexOf(value);
-    const newChecked = [...checked];
-
-    if (currentIndex === -1) {
-      newChecked.push(value);
-    } else {
-      newChecked.splice(currentIndex, 1);
-    }
-
-    setChecked(newChecked);
-  };
-
   return (
     <List dense sx={{ width: '100%', bgcolor: 'background.paper' }}>
       {users.map((user) => {
-        const labelId = `checkbox-list-secondary-label-${user}`;
         return (
             <Link href={`/room?id=${user.id}`}>
                 <ListItem key={user.id}>
@@ -39,7 +23,7 @@ export default function IdeikoUserList({users}) {
                             src={`/static/images/avatar/${user.id}.jpg`}
                             />
                         </ListItemAvatar>
-                    <ListItemText id={labelId} primary={user.name} />
+                    <ListItemText primary={user.name} />
                     </ListItemButton>
                 </ListItem>
             </Link>
